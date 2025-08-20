41233f30375278fabbdbd1733b3f03c6883d368d
brxnxpxdrx <newbrd3v@gmail.com>
2025-08-19 21:40:21 -0300

    algumas configuralçoes da criação de personagens

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
-app.express.json()
+app.use(express.json())
 
 
+app.get('/usuarios', async (req,res) =>{
+    const allUsers = await prisma.user.findMany()
+    res.json(allUsers)
+})
+
+app.post('/usuarios', async (req,res) =>{
+    const {nick, email, password } = req.body
+
+    const newUser = await prisma.user.create({
+        data: {
+           nick,
+            email,
+            password
+        }
+    })
+
+    res.json(newUser)
+})
 
 app.listen(3000,() => console.log('Server is running on port 3000'))
