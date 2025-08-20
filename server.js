68cd830addac406d4c4f8c31366b25220e54fd79
brxnxpxdrx <newbrd3v@gmail.com>
2025-08-19 21:50:20 -0300

    crud de usuários completo

--- server.js
+++ server.js
@@ -26,4 +26,29 @@ app.post('/usuarios', async (req,res) =>{
     res.json(newUser)
 })
 
+
+app.put('/usuarios/:id', async (req,res) =>{
+    const id = req.params.id
+    const {nick, email, password } = req.body
+
+    const updatedUser = await prisma.user.update({
+        where: { id },
+        data: {
+            nick,
+            email,
+            password
+        }
+    })
+    res.send(200).json("Usuário atualizado com sucesso")
+})
+
+app.delete('/usuarios/:id', async (req,res) =>{
+    const id = req.params.id
+
+    await prisma.user.delete({
+        where: { id }
+    })
+
+    res.send(200).json("Usuário deletado com sucesso")
+})
 app.listen(3000,() => console.log('Server is running on port 3000'))
